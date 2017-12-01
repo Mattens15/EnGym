a7be36e784d32dd3d88d976b577f3a4ecc9e2d51
Mattens15 <matteo.attenni@outlook.it>
2017-12-01 19:49:35 +0100

    ripristinata la repo

---
+++ Now.js
@@ -0,0 +1,20 @@
+var c={ 1:["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g"],
+        2:["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g"],
+        3:["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g"],
+        4:["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g"],
+        5:["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g"],
+        6:["q","w","e","r","t","y","u","i","p","a","s","d","f","g"],
+        7:["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g"]
+      };
+
+function attivita(){
+  var d = new Date();
+  var x= d.getDay();
+  var h=d.getHours();
+  if(h>22||h<8){
+    document.getElementById('msg').innerHTML="Palestra chiusa";
+  }
+  else{
+    document.getElementById('msg').innerHTML=c[x][h-8];
+  }
+}
